d42d563e7846450c3fe877e2fec535201a2df33b
Robert <robertodonnell76@yahoo.ie>
2023-02-14 14:49:57 +0000

    Commiting changes

--- src/test/java/tudelft/gettingstarted/GettingStartedTest.java
+++ src/test/java/tudelft/gettingstarted/GettingStartedTest.java
@@ -1,5 +1,6 @@
 package tudelft.gettingstarted;
 
+//import com.sun.org.apache.xpath.internal.objects.XNumber;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -13,15 +14,15 @@ public class GettingStartedTest {
 
 //    UNCOMMENT THE CODE BELOW, AND FILL THE GAPS!
 
-//    @Test
-//    public void addFiveToZero() {
-//        int result = new GettingStarted().addFive(???);
-//        Assertions.assertEquals(???, result);
-//    }
-//
-//    @Test
-//    public void addFiveToMinus20() {
-//        int result = new GettingStarted().addFive(???);
-//        Assertions.assertEquals(????,result);
-//    }
+    @Test
+    public void addFiveToZero() {
+           int result = new GettingStarted().addFive(0);
+        Assertions.assertEquals(5, result);
+    }
+
+   @Test
+   public void addFiveToMinus20() {
+        int result = new GettingStarted().addFive(-20);
+        Assertions.assertEquals(-15,result);
+    }
 }
